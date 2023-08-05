7478f62fb3c0f1a907f960c547ae61918c1b4248
saed <samersame404@gmail.com>
2023-08-05 10:09:16 +0300

    Initial commit

---
+++ lib/circle_image_widget.dart
@@ -0,0 +1,45 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+
+import 'image_multi_type.dart';
+
+class CircleImageWidget extends StatelessWidget {
+  const CircleImageWidget({
+    Key? key,
+    this.size,
+    required this.url,
+    this.color,
+    this.margin,
+    this.padding,
+    this.file,
+  }) : super(key: key);
+
+  final double? size;
+  final String? url;
+  final Future<Uint8List>? file;
+  final Color? color;
+  final EdgeInsets? margin;
+  final EdgeInsets? padding;
+
+  @override
+  Widget build(BuildContext context) {
+    final imageRadios = size ?? 150.0;
+    return Container(
+      height: imageRadios,
+      width: imageRadios,
+      margin: margin,
+      clipBehavior: Clip.hardEdge,
+      padding: padding,
+      decoration: BoxDecoration(
+        color: color ?? Colors.white,
+        shape: BoxShape.circle,
+      ),
+      child: ImageMultiType(
+        url: url ?? '',
+        file: file,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
